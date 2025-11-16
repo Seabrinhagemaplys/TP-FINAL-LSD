44dc82a05ab1d6e3fb1733f374db898645caa6d1
Lucas-Szuster <lucasacszuster@gmail.com>
2025-11-15 21:54:07 -0300

    Update testbench.vhd
    Adicionei um testebench mais completo

--- blocos_logicos/alternador/testbench.vhd
+++ blocos_logicos/alternador/testbench.vhd
@@ -1,5 +1,6 @@
 library IEEE;
 use IEEE.std_logic_1164.all;
+
 entity tb_alternador is
 end entity;
 
@@ -9,25 +10,33 @@ architecture arq of tb_alternador is
     signal clk        : std_logic := '1';
     signal clk_enable : std_logic := '1';
     constant tempo    : time := 10 ns;
+begin
+	-- inicializando a uut e realizando o port map
+	uut:entity work.alternador
+    
+    port map(
+		entrada => entrada,
+        saida => saida,
+		clk => clk
+	);
+    
+    -- inicializando o pulso do clock
+    clk <= clk_enable and not clk after tempo/2;
+    
+    stim: process
     begin
-		uut:entity work.alternador
-        	port map(
-            	entrada => entrada,
-                saida => saida,
-                clk => clk
-            );
-            clk <= clk_enable and not clk after tempo/2;
-            process
-            begin
-            entrada <= '1'; wait for tempo*5;
-            entrada <= '0'; wait for tempo*5;
-            entrada <= '1'; wait for tempo*5;
+    	-- estímulos de entrada
+    	entrada <= '1'; wait for tempo*5;
+        entrada <= '0'; wait for tempo*5;
+        entrada <= '1'; wait for tempo*10;
+        entrada <= '0'; wait for tempo*5;
+        entrada <= '1'; wait for tempo*20;
+        entrada <= '0'; wait for tempo*5;
             
-            clk_enable <= '0';
+        -- finaliza o clock
+        clk_enable <= '0';
 
-        wait;  -- termina simulação
+		-- termina simulação
+        wait;  
     end process;
 end architecture;
-            
-            
-    
