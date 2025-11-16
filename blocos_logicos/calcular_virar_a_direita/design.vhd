b1a5c1d1bdf8970c5db4b388a1a4b53adc9e274b
Lucas-Szuster <lucasacszuster@gmail.com>
2025-11-15 22:25:57 -0300

    Update design.vhd
    Atualizei o bloco de virar a direita para conter os sinais de emergencia e outra emergência

--- blocos_logicos/calcular_virar_a_direita/design.vhd
+++ blocos_logicos/calcular_virar_a_direita/design.vhd
@@ -6,6 +6,8 @@ entity calcular_virar_a_direita is
     -- entradas
 	pedestre : in std_logic;
     outro_pedestre : in std_logic;
+    emergencia : in std_logic;
+    outra_emergencia : in std_logic;
     
     -- saídas
     virar_a_direita : out std_logic := '0'
@@ -15,5 +17,5 @@ end entity;
 architecture calc of calcular_virar_a_direita is
 begin
 	-- pode virar a direita se nenhum pedestre está atravessando
-	virar_a_direita <= not(pedestre) and not(outro_pedestre);	
+	virar_a_direita <= not(pedestre) and not(outro_pedestre) and not(emergencia) and not(outra_emergencia);	
 end architecture;
