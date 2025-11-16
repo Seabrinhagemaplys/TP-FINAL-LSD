8d969ee3fab3108b0b0dfc2104e20a43a602c6ac
Lucas-Szuster <lucasacszuster@gmail.com>
2025-11-15 21:53:11 -0300

    Update design.vhd
    Corrigi alguns erros na implementação do alternador

--- blocos_logicos/alternador/design.vhd
+++ blocos_logicos/alternador/design.vhd
@@ -2,26 +2,32 @@ library IEEE;
 use IEEE.std_logic_1164.all;
 
 entity alternador is
-    --Declaração das entradas e saídas
     port(
+        -- entradas
         entrada : in  std_logic := '0';
-        saida   : out std_logic;
-        clk     : in  std_logic
+        clk     : in  std_logic;
+
+        -- saídas
+        saida   : out std_logic := '0'
     );
 end entity;
 
 architecture arq of alternador is
-    --Sinal que será sempre invertido
-    signal inversor : std_logic := '0';
+    signal inversor : std_logic := '1'; 
 begin
     process(clk)
     begin
         if rising_edge(clk) then
-            if entrada = '1' then -- Só realiza processo caso entrada for 1      
-            inversor <= not inversor;
+            if entrada = '1' then
+                -- alterna o valor de inversor quando a entrada for '1'
+                inversor <= not inversor;
+            else
+            	-- resetar o inversor para '1'
+            	inversor <= '1';
             end if;
         end if;
     end process;
 
-    saida <= inversor;
+    -- a saída é o valor de inversor quando entrada = '1'
+    saida <= inversor when entrada = '1' else '0';
 end architecture;
